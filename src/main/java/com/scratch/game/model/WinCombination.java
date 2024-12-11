81097a37985d118368eb971302f8289d1c66c23d
marcelaschacon <42355150+marcelaschacon@users.noreply.github.com>
2024-12-11 13:00:08 -0300

    Update WinCombination.java

--- src/main/java/com/scratch/game/model/WinCombination.java
+++ src/main/java/com/scratch/game/model/WinCombination.java
@@ -6,11 +6,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class WinCombination {
 	
-		@JsonProperty("reward_multiplier")
-		public double rewardMultiplier;
-		@JsonProperty("when")
+	@JsonProperty("reward_multiplier")
+	public double rewardMultiplier;
+	@JsonProperty("when")
         public String when;
-		 @JsonProperty("count")
+ 	@JsonProperty("count")
         public int count;
         @JsonProperty("group")
         public String group;
@@ -18,44 +18,44 @@ public class WinCombination {
         public List<List<String>> coveredAreas;
         
         public double getRewardMultiplier() {
-			return rewardMultiplier;
-		}
+		return rewardMultiplier;
+	}
 
-		public void setRewardMultiplier(double rewardMultiplier) {
-			this.rewardMultiplier = rewardMultiplier;
-		}
+	public void setRewardMultiplier(double rewardMultiplier) {
+		this.rewardMultiplier = rewardMultiplier;
+	}
 
-		public String getWhen() {
-			return when;
-		}
+	public String getWhen() {
+		return when;
+	}
 
-		public void setWhen(String when) {
-			this.when = when;
-		}
+	public void setWhen(String when) {
+		this.when = when;
+	}
 
-		public int getCount() {
-			return count;
-		}
+	public int getCount() {
+		return count;
+	}
 
-		public void setCount(int count) {
-			this.count = count;
-		}
+	public void setCount(int count) {
+		this.count = count;
+	}
 
-		public String getGroup() {
-			return group;
-		}
+	public String getGroup() {
+		return group;
+	}
 
-		public void setGroup(String group) {
-			this.group = group;
-		}
+	public void setGroup(String group) {
+		this.group = group;
+	}
 
-		public List<List<String>> getCoveredAreas() {
-			return coveredAreas;
-		}
+	public List<List<String>> getCoveredAreas() {
+		return coveredAreas;
+	}
 
-		public void setCoveredAreas(List<List<String>> coveredAreas) {
-			this.coveredAreas = coveredAreas;
-		}
+	public void setCoveredAreas(List<List<String>> coveredAreas) {
+		this.coveredAreas = coveredAreas;
+	}
 
 		@Override
         public String toString() {
